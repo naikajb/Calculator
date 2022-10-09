04c8aa9d9c79b6ce505ceb645b8244be57fc2f45
naikajb <jbaptiste.naika@gmail.com>
2022-10-09 19:59:39 -0400

    Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+/*
+
+
+ */
+public class Calculator {
+
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel text = new JLabel();
+    JButton[] buttons = new JButton[9];
+
+
+}
